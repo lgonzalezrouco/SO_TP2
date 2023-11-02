517957d8a6605e32a5078857dab057e51d246525
lgonzalezrouco <lgonzalezrouco@itba.edu.ar>
2023-11-02 10:36:37 -0300

    style: :art: format code

--- Kernel/include/processes.h
+++ Kernel/include/processes.h
@@ -8,22 +8,20 @@
 
 #define STACK_SIZE 4096
 
-typedef enum processStatus {
-    RUNNING, BLOCKED, KILLED, READY
-} processStatus;
+typedef enum processStatus { RUNNING, BLOCKED, KILLED, READY } processStatus;
 
 typedef struct PCB {
-    uint16_t pid;
-    uint16_t parentPid;
-    char *name;
-    void *basePointer;
-    void *stackPointer;
-    uint8_t priority;
-    processStatus status;
+  uint16_t	pid;
+  uint16_t	parentPid;
+  char	       *name;
+  void	       *basePointer;
+  void	       *stackPointer;
+  uint8_t	priority;
+  processStatus status;
 } PCB;
 
 int createProcess(uint16_t parentPid, int (*func)(int, char **), char **args,
-                  char *name, uint8_t priority);
+		  char *name, uint8_t priority);
 
 void *setup_stack(void *wrapper, void *rip, void *rsp, void *args);
 
