00da8cc4829534dc5cc21093a2a92b52ca33d7c8
lgonzalezrouco <lgonzalezrouco@itba.edu.ar>
2023-11-09 18:11:37 -0300

    style: :art: format code
    Co-authored-by: Matias Mutz <mmutz@itba.edu.ar>

--- Kernel/include/semaphores.h
+++ Kernel/include/semaphores.h
@@ -6,38 +6,38 @@
 typedef int sem_t;
 
 typedef struct Semaphore {
-    char name[50];
-    sem_t id;
-    uint32_t value;
+	char name[50];
+	sem_t id;
+	uint32_t value;
 } Semaphore;
 
 void initializeSemaphores();
 
 /**
  * @brief Crea un nuevo semaforo, indentificado por name y con valor inicial value
- * 
+ *
  * @attention El nombre del semaforo debe ser unico
- * 
+ *
  * @param name: Nombre con el que se identifica el semaforo
  * @param value: Valor inicial del semaforo
  * @return 0 si se creo correctamente, -1 si ya existe un semaforo con ese nombre
  */
-int sem_init(const char *name, uint32_t value);
+int sem_init(const char * name, uint32_t value);
 
 /**
  * @brief Abre un semaforo con el nombre name
- * 
+ *
  * @param name: Nombre del semaforo
  * @return 0 si se abrio correctamente, -1 si no se pudo abrir
  */
-int sem_open(const char *name);
+int sem_open(const char * name);
 
 /**
  * @brief Cierra el semaforo indicado
- * 
+ *
  * @param name: nombre del semaforo a cerrar
  * @return 0 si se borro correctamente, -1 si no se pudo borrar
  */
-int sem_close(const char *name);
+int sem_close(const char * name);
 
 #endif
--- Kernel/semaphores/semaphores.c
+++ Kernel/semaphores/semaphores.c
@@ -4,65 +4,65 @@
 
 #define MAX_SEMAPHORES 1024
 
-static Semaphore * getSemaphore(const char *name);
+static Semaphore * getSemaphore(const char * name);
 static int getNextSemaphoreID();
 static void freeSemaphores(Semaphore * semaphore);
 
-Semaphore* semaphores[MAX_SEMAPHORES];
+Semaphore * semaphores[MAX_SEMAPHORES];
 
 void initializeSemaphores() {
-    for (int i = 0; i < MAX_SEMAPHORES; i++)
-        semaphores[i] = NULL;
+	for (int i = 0; i < MAX_SEMAPHORES; i++)
+		semaphores[i] = NULL;
 }
 
-int sem_init(const char *name, uint32_t value) { 
-    if (getSemaphore(name) != NULL)
-        return -1;
-    
-    int id = getNextSemaphoreID();
-    if (id == -1)
-        return -1;
+int sem_init(const char * name, uint32_t value) {
+	if (getSemaphore(name) != NULL)
+		return -1;
 
-    semaphores[id] = (Semaphore *) malloc(sizeof(Semaphore));
-    if (semaphores[id] == NULL)
-        return -1;
-    
-    strcpy(semaphores[id]->name, name);
-    semaphores[id]->id = id;
-    semaphores[id]->value = value;
+	int id = getNextSemaphoreID();
+	if (id == -1)
+		return -1;
 
-    return 0;
+	semaphores[id] = (Semaphore *) malloc(sizeof(Semaphore));
+	if (semaphores[id] == NULL)
+		return -1;
+
+	strcpy(semaphores[id]->name, name);
+	semaphores[id]->id = id;
+	semaphores[id]->value = value;
+
+	return 0;
 }
 
-int sem_open(const char *name) { }
-
-int sem_close(const char *name) { 
-    Semaphore * semaphore = getSemaphore(name);
-    if (semaphore == NULL)
-        return -1;
-    
-    freeSemaphore(semaphore);
-    semaphores[semaphore->id] = NULL;
-    return 0;
+int sem_open(const char * name) {
 }
 
-static Semaphore * getSemaphore(const char *name) {
-    for (int i = 0; i < MAX_SEMAPHORES; i++) {
-        if (semaphores[i] != NULL && strcmp(semaphores[i]->name, name) == 0)
-            return semaphores[i];
-    }
-    return NULL;
+int sem_close(const char * name) {
+	Semaphore * semaphore = getSemaphore(name);
+	if (semaphore == NULL)
+		return -1;
 
+	freeSemaphore(semaphore);
+	semaphores[semaphore->id] = NULL;
+	return 0;
+}
+
+static Semaphore * getSemaphore(const char * name) {
+	for (int i = 0; i < MAX_SEMAPHORES; i++) {
+		if (semaphores[i] != NULL && strcmp(semaphores[i]->name, name) == 0)
+			return semaphores[i];
+	}
+	return NULL;
 }
 
 static int getNextSemaphoreID() {
-    for (int i = 0; i < MAX_SEMAPHORES; i++) {
-        if (semaphores[i] == NULL)
-            return i;
-    }
-    return -1;
+	for (int i = 0; i < MAX_SEMAPHORES; i++) {
+		if (semaphores[i] == NULL)
+			return i;
+	}
+	return -1;
 }
 
-static void freeSemaphores(Semaphore * semaphore) { 
-    free(semaphore);
+static void freeSemaphores(Semaphore * semaphore) {
+	free(semaphore);
 }
